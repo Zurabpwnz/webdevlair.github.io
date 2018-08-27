a91e3dfc043b0ba7a4c4487defb571175aed444b
Zurab Shivarbidze <zurab@artlog,pro>
2018-08-27 20:30:02 +0300

    added files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+;
+$(document).ready(function(){
+
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsiO1xyXG4kKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpe1xyXG5cclxufSk7Il0sImZpbGUiOiJhcHAuanMifQ==
